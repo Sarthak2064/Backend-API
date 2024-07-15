6a4f6e788b7c26673756d51f94bee34d65454915
Sarthak Gajendragadkar <146093758+Sarthak2064@users.noreply.github.com>
2024-07-15 20:11:46 +0530

    Update sayHelloAPI.js

--- hello-api/sayHelloAPI.js
+++ hello-api/sayHelloAPI.js
@@ -1,1 +1,11 @@
-hi
+const express = require("express");
+const app = express();
+
+app.get('/sayHello', (req, res) => {
+    res.json({ message: "Hello User" });
+});
+
+const PORT = 80;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
